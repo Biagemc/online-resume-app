61b6598c61c7d49e3fcc216eefa6fadff3591f96
biagemc <biagemc@gmail.com>
2020-05-04 22:27:44 -0400

    add resume profile setup

--- src/views/ResumeShow.vue
+++ src/views/ResumeShow.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <h1>{{ message }}</h1>
 
-    <!-- <div class="main main-raised">
+    <div class="main main-raised">
       <div class="profile-content">
         <div class="container">
           <div class="row">
@@ -10,7 +10,9 @@
               <div class="profile">
                 <div class="avatar">
                   <img
-                    src="../public/assets/img/examples/cynthia-del-rio.jpg"
+                    v-bind:src="
+                      'https://avatars0.githubusercontent.com/u/61444123?s=460&u=cbf80e36ca02b777f8be3f04e25507647f29d19b&v=4'
+                    "
                     alt="Circle Image"
                     class="img-raised rounded-circle img-fluid"
                   />
@@ -38,8 +40,8 @@
                 <ul class="nav nav-pills nav-pills-icons justify-content-center" role="tablist">
                   <li class="nav-item">
                     <a class="nav-link active" href="#studio" role="tab" data-toggle="tab">
-                      <i class="fas fa-user-edit"></i>
-                      Studio
+                      <i class="material-icons">face</i>
+                      Personal
                     </a>
                   </li>
                   <li class="nav-item">
@@ -62,45 +64,45 @@
             <div class="tab-pane active text-center gallery" id="studio">
               <div class="row">
                 <div class="col-md-3 ml-auto">
-                  <img src="../public/assets/img/examples/studio-1.jpg" class="rounded" />
-                  <img src="../public/assets/img/examples/studio-2.jpg" class="rounded" />
+                  <img v-bind:src="'./public/assets/img/examples/studio-1.jpg'" class="rounded" />
+                  <img v-bind:src="'./public/assets/img/examples/studio-2.jpg'" class="rounded" />
                 </div>
                 <div class="col-md-3 mr-auto">
-                  <img src="../public/assets/img/examples/studio-5.jpg" class="rounded" />
-                  <img src="../public/assets/img/examples/studio-4.jpg" class="rounded" />
+                  <img v-bind:src="'./public/assets/img/examples/studio-5.jpg'" class="rounded" />
+                  <img v-bind:src="'./public/assets/img/examples/studio-4.jpg'" class="rounded" />
                 </div>
               </div>
             </div>
             <div class="tab-pane text-center gallery" id="works">
               <div class="row">
                 <div class="col-md-3 ml-auto">
-                  <img src="../public/assets/img/examples/olu-eletu.jpg" class="rounded" />
-                  <img src="../public/assets/img/examples/clem-onojeghuo.jpg" class="rounded" />
-                  <img src="../public/assets/img/examples/cynthia-del-rio.jpg" class="rounded" />
+                  <img v-bind:src="'./public/assets/img/examples/olu-eletu.jpg'" class="rounded" />
+                  <img v-bind:src="'./public/assets/img/examples/clem-onojeghuo.jpg'" class="rounded" />
+                  <img v-bind:src="'./public/assets/img/examples/cynthia-del-rio.jpg'" class="rounded" />
                 </div>
                 <div class="col-md-3 mr-auto">
-                  <img src="../public/assets/img/examples/mariya-georgieva.jpg" class="rounded" />
-                  <img src="../public/assets/img/examples/clem-onojegaw.jpg" class="rounded" />
+                  <img v-bind:src="'./public/assets/img/examples/mariya-georgieva.jpg'" class="rounded" />
+                  <img v-bind:src="'./public/assets/img/examples/clem-onojegaw.jpg'" class="rounded" />
                 </div>
               </div>
             </div>
             <div class="tab-pane text-center gallery" id="favorite">
               <div class="row">
                 <div class="col-md-3 ml-auto">
-                  <img src="../assets/img/examples/mariya-georgieva.jpg" class="rounded" />
-                  <img src="../assets/img/examples/studio-3.jpg" class="rounded" />
+                  <img v-bind:src="'./assets/img/examples/mariya-georgieva.jpg'" class="rounded" />
+                  <img v-bind:src="'./assets/img/examples/studio-3.jpg'" class="rounded" />
                 </div>
                 <div class="col-md-3 mr-auto">
-                  <img src="../assets/img/examples/clem-onojeghuo.jpg" class="rounded" />
-                  <img src="../assets/img/examples/olu-eletu.jpg" class="rounded" />
-                  <img src="../assets/img/examples/studio-1.jpg" class="rounded" />
+                  <img v-bind:src="'./assets/img/examples/clem-onojeghuo.jpg'" class="rounded" />
+                  <img v-bind:src="'./assets/img/examples/olu-eletu.jpg'" class="rounded" />
+                  <img v-bind:src="'./assets/img/examples/studio-1.jpg'" class="rounded" />
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div> -->
+    </div>
   </div>
 </template>
 
